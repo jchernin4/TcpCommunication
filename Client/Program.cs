a68a306e1eb9f7c4555debefcc25e7c790012b0e
jchernin4 <jeremychernin@gmail.com>
2021-06-07 13:18:01 -0400

    Working

---
+++ Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Starting...");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            while (!socket.Connected) {
+                try {
+                    socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4342));
+                } catch (Exception) {
+                    // ignored
+                }
+                Thread.Sleep(5000);
+            }
+            
+            Console.WriteLine("Connected!");
+            while (true) {
+                byte type = (byte) 1;
+                byte[] textBytes = Encoding.ASCII.GetBytes("This is a test!");
+                byte[] length = BitConverter.GetBytes(textBytes.Length);
+                byte[] final = new byte[5 + textBytes.Length];
+                final[0] = type;
+                
+                Array.Copy(length, 0, final, 1, 4);
+                Array.Copy(textBytes, 0, final, 5, textBytes.Length);
+                
+                socket.Send(final);
+                
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server {
+    [SuppressMessage("ReSharper", "ArrangeObjectCreationWhenTypeEvident")]
+    public class Program {
+        public class StateObject {
+            public byte[] buffer;
+            public Socket socket;
+        }
+        
+        public static void Main(string[] args) {
+            Console.WriteLine("Starting...");
+
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(new IPEndPoint(IPAddress.Any, 4342));
+            listener.Listen(10);
+            while (true) {
+                listener.BeginAccept(AcceptCallback, listener);
+                Thread.Sleep(5000);
+            }
+        }
+
+        private static void AcceptCallback(IAsyncResult ar) {
+            Socket listener = (Socket)ar.AsyncState;
+            if (listener != null) {
+                Socket handler = listener.EndAccept(ar);
+
+                Console.WriteLine("Client connected HERE");
+
+                StateObject so = new StateObject();
+                so.socket = handler;
+                so.buffer = new byte[5];
+                handler.BeginReceive(so.buffer, 0, so.buffer.Length, 0, ReceiveCallback, so);
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar) {
+            StateObject so = (StateObject)ar.AsyncState;
+            Socket handler = so.socket;
+            int bytesRead = handler.EndReceive(ar);
+            if (bytesRead > 0) {
+                byte type = so.buffer[0];
+                int length = BitConverter.ToInt32(so.buffer, 1); // Skip first byte, find a 4 byte int
+                
+                Console.WriteLine("Type " + type);
+                Console.WriteLine("Length " + length);
+                
+                switch (type) {
+                    case 1:
+                        so.buffer = new byte[length];
+                        handler.Receive(so.buffer, SocketFlags.None);
+                        string result = Encoding.ASCII.GetString(so.buffer);
+                        Console.WriteLine("Received " + result);
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Unknown type " + type);
+                        break;
+                }
+
+                so.buffer = new byte[5];
+                handler.BeginReceive(so.buffer, 0, so.buffer.Length, 0, ReceiveCallback, so);
+            }
+        }
+    }
+}
